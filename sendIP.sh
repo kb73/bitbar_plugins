0315bdeb4d9ab2bdd27a4cd898ecdf205c17acea
Kevin Bowers <kevin.p.bowers@gmail.com>
2019-09-12 11:31:45 -0500

    Reformatted

--- sendIP.sh
+++ sendIP.sh
@@ -7,18 +7,18 @@ echo "---"
 #echo "Send ip | bash=iptodrive terminal=false "
 #echo "---"
 # Check to see if wifi interface exists
-    if [ "$(ifconfig | grep 'en0')" = "" ]; then
-        wifiAddress="not connected"
-    else
-        wifiAddress=$(ifconfig en0 | grep 'inet ' | awk '{print $2}')
+if [ "$(ifconfig | grep 'en0')" = "" ]; then
+    wifiAddress="not connected"
+else
+    wifiAddress=$(ifconfig en0 | grep 'inet ' | awk '{print $2}')
     fi
 
-    # Check to see if ethernet interface exists
-    if [ "$(ifconfig | grep 'en12')" = "" ]; then
-        ethernetAddress="not connected"
-    else
-        ethernetAddress=$(ifconfig en12 | grep 'inet ' | awk '{print $2}')
-    fi
+# Check to see if ethernet interface exists
+if [ "$(ifconfig | grep 'en12')" = "" ]; then
+    ethernetAddress="not connected"
+else
+    ethernetAddress=$(ifconfig en12 | grep 'inet ' | awk '{print $2}')
+fi
 
-	echo -e "wifi\t\t$wifiAddress"
-	echo -e "enet\t$ethernetAddress"
+echo -e "wifi\t\t$wifiAddress"
+echo -e "enet\t$ethernetAddress"
