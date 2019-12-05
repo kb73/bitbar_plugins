3809c871f807845189e7002e7a04f1f67b6896fb
Kevin Bowers <kevin.p.bowers@gmail.com>
2019-12-05 11:56:54 -0600

    Updated methods and error handling
    Changed the way the ip address is gotten. Now using 'ipconfig getifaddr <iface>'. Just checks for error to know if iface is not connected

--- ip.sh
+++ ip.sh
@@ -2,22 +2,25 @@
 
 #source ~/.custom_commands.sh <- this was needed when "ip" was the command that was run instead of typing out the code from the command here
 
-# Check to see if wifi interface exists
-if [ "$(ifconfig en0 | grep inet )" = "" ]; then
+# Set variable 'wifiAddress' to the address of interface en0 (known to be ethernet iface for this mac).
+# '2> /dev/null' throws aways any error messages
+wifiAddress=$(ipconfig getifaddr en0 2> /dev/null)
+if [ $? -eq 1 ]; then
+
+	# If previous command exited with an error, set 'wifiAddress' to 'not connected'
     wifiAddress="not connected"
-else
-    wifiAddress=$(ifconfig en0 | grep 'inet ' | awk '{print $2}')
 fi
 
-# Check to see if ethernet interface exists
-if [ "$(ifconfig en5 2> /dev/null | grep inet )" = "" ]; then
+# Set variable 'ethernetAddress' to the address of interface en5 (known to be ethernet iface for this mac).
+# '2> /dev/null' throws aways any error messages
+ethernetAddress=$(ipconfig getifaddr en5 2> /dev/null)
+
+# If previous command exited with an error, set 'ethernetAddress' to 'not connected'
+if [ $? -eq 1 ]; then
     ethernetAddress="not connected"
-else
-	## if ethernet connected, get address
-    ethernetAddress=$(ifconfig en5 | grep 'inet ' | awk '{print $2}')
 fi
 
-shortip=$(echo $wifiAddress | cut -d'.' -f 3,4)
+shortip=$(echo $wifiAddress | cut -d'.' -f 3,4) # Last 2 #s of wifi ip to display to menu bar
 
 echo "$shortip | color=#0000FF"
 echo ---
