428f2aa794761b933fb0a70d9d603feb295b7619
Kevin Bowers <kevin.p.bowers@gmail.com>
2019-12-13 15:14:16 -0600

    Udpated ip.sh
    Turns wifi off if ethernet is connected. Turns wifi back on and updates caffeinate plugin if "Save and caffeinate" is selected. Differentiates between the wifi being off or something else.  Writes to a file on desktop every times it updates (used to see how frequently it updates when no interval is specified)

--- ip.sh
+++ ip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 #source ~/.custom_commands.sh <- this was needed when "ip" was the command that was run instead of typing out the code from the command here
 
 # Set variable 'wifiAddress' to the address of interface en0 (known to be ethernet iface for this mac).
@@ -7,8 +9,12 @@
 wifiAddress=$(ipconfig getifaddr en0 2> /dev/null)
 if [ $? -eq 1 ]; then
 
-	# If previous command exited with an error, set 'wifiAddress' to 'not connected'
-    wifiAddress="no wifi"
+    if [ "$(networksetup -getairportpower en0 | grep off)" = "" ]; then
+        wifiAddress="wifi off"
+    else
+	    # If previous command exited with an error, set 'wifiAddress' to 'not connected'
+        wifiAddress="no wifi"
+    fi
     
 fi
 
@@ -19,11 +25,33 @@ ethernetAddress=$(ipconfig getifaddr en5 2> /dev/null)
 # If previous command exited with an error, set 'ethernetAddress' to 'not connected'
 if [ $? -eq 1 ]; then
     ethernetAddress="not connected"
+else
+    networksetup -setairportpower en0 off
+fi
+
+# If Save option clicked, caffeinate and save wifi address to google_drive/ip.txt
+if [ "$1" = "save" ]; then
+    sudo -S pmset -b sleep 0 && sudo -S pmset -b disablesleep 1
+
+    # If wifi is off, turn it on
+    if [ "$(networksetup -getairportpower en0 | grep On)" = "" ]; then
+        networksetup -setairportpower en0 on
+        # Wait to set 'wifiAddress' variable until an ip address is available
+        while [ "$(ifconfig en0 | grep inet)" = "" ]; do
+            sleep 2
+        done
+        wifiAddress=$(ipconfig getifaddr en0)
+    fi
+    echo $wifiAddress > ~/google_drive/ip.txt
+    open bitbar://refreshPlugin?name=caffeine.5s.sh
 fi
 
 shortip=$(echo $wifiAddress | cut -d'.' -f 3,4) # Last 2 #s of wifi ip to display to menu bar
 
 echo "$shortip | color=#0000FF"
+echo ---
+
+echo "Save and ☕️ | bash=$0 param1=save terminal=false "
 echo ---
 
 #echo "Send ip | bash=iptodrive terminal=false " <- ip to drive is old function that doesn't work
@@ -32,3 +60,4 @@ echo -e "wifi\t\t$wifiAddress"
 echo -e "enet\t$ethernetAddress"
 echo ---
 echo "Refresh | refresh=true"
+echo "ip updated: $(date)" >> ~/Desktop/bitbar_update_log.txt
