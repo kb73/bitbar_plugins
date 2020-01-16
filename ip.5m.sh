f994b96d2c449bb9784c09b13aca6bddb821b7d8
Kevin Bowers <kevin.p.bowers@gmail.com>
2020-01-16 17:09:08 -0600

    Added wifi auto off toggle

--- ip.5m.sh
+++ ip.5m.sh
@@ -2,12 +2,32 @@
 
 #source ~/.custom_commands.sh <- this was needed when "ip" was the command that was run instead of typing out the code from the command here
 
+#Enable/disable flag for automatically turning wifi off if connected to ethernet
+
+
+
+if [ "$1" == "toggleAutomaticOff" ]; then
+    isEnabled=$(cat ~/bitbar_plugins/support_files/isEnabled)
+    if [ "$isEnabled" == "true" ]; then
+        echo "false" > ~/bitbar_plugins/support_files/isEnabled
+    else
+        echo "true" > ~/bitbar_plugins/support_files/isEnabled
+    fi
+fi
+
+isEnabled=$(cat ~/bitbar_plugins/support_files/isEnabled)
+    if [ "$isEnabled" == "true" ]; then
+        automaticOffText="Disable"
+    else
+        automaticOffText="Enable"
+    fi
+
 # Set variable 'wifiAddress' to the address of interface en0 (known to be ethernet iface for this mac).
 # '2> /dev/null' throws aways any error messages
 wifiAddress=$(ipconfig getifaddr en0 2> /dev/null)
 if [ $? -eq 1 ]; then
 
-    if [ "$(networksetup -getairportpower en0 | grep off)" = "" ]; then
+    if [ "$(networksetup -getairportpower en0 | grep On)" = "" ]; then
         wifiAddress="wifi off"
     else
 	    # If previous command exited with an error, set 'wifiAddress' to 'not connected'
@@ -24,7 +44,10 @@ ethernetAddress=$(ipconfig getifaddr en5 2> /dev/null)
 if [ $? -eq 1 ]; then
     ethernetAddress="not connected"
 else
-    networksetup -setairportpower en0 off
+    if [ "$isEnabled" == "true" ]; then
+        networksetup -setairportpower en0 off
+    fi
+    
 fi
 
 # If Save option clicked, caffeinate and save wifi address to google_drive/ip.txt
@@ -48,20 +71,32 @@ fi
 shortip=$(echo $wifiAddress | cut -d'.' -f 3,4) # Last 2 #s of wifi ip to display to menu bar
 
 echo "$shortip | color=#0000FF"
+
 echo ---
 
 echo "Save and ☕️ | bash=$0 param1=save terminal=false "
-echo ---
 
-#echo "Send ip | bash=iptodrive terminal=false " <- ip to drive is old function that doesn't work
+echo ---
 
 echo -e "wifi\t\t$wifiAddress"
 echo -e "enet\t$ethernetAddress"
-echo ---
-echo "Refresh | refresh=true"
 
 echo ---
+
+if [ "$automaticOffText" == "Enable" ]; then
+    echo "Wifi auto off: Disabled"
+else
+    echo "Wifi auto off: Enabled"
+fi
+echo "$automaticOffText auto off | bash=$0 param1=toggleAutomaticOff terminal=false refresh=true "
+
+echo ---
+
+echo "Refresh | refresh=true"
 echo "Edit plugin | bash=$0 param1=edit terminal=false "
 if [ "$1" = "edit" ]; then
     open "${0}"
 fi
+
+#TODO
+#Add enable and disable feature to the code that automatically turns off wifi when ethernet is connected
